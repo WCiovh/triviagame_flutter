738a90ea0d55cb7b434aefe8539feab4b2443ee9
Arkadiusz Cios <arkadiuszcios@outlook.com>
2026-05-27 19:58:13 +0200

    feat(main): configure prod backend url

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -5,8 +5,8 @@ class AppConfig {
   static const String apiKey = String.fromEnvironment('API_KEY', defaultValue: '');
   static String get baseUrl {
     if (kIsWeb) return 'https://trivia.arkadiuszcios.online';
-    if (Platform.isAndroid) return 'http://192.168.33.9:5114';
-    return 'http://localhost:5114';
+    if (Platform.isAndroid) return 'https://trivia.arkadiuszcios.online';
+    return 'https://trivia.arkadiuszcios.online';
   }
 
   static String get hubUrl => '$baseUrl/hubs/game';
