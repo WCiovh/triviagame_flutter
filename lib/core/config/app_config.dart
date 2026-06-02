a079e8e8e67d727159dbee1139ea35c6a8a6ffef
Arkadiusz Cios <arkadiuszcios@outlook.com>
2026-06-02 11:08:02 +0200

    fix(main): backend service url fix

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,9 +4,9 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class AppConfig {
   static const String apiKey = String.fromEnvironment('API_KEY', defaultValue: '');
   static String get baseUrl {
-    if (kIsWeb) return 'http://localhost:5114';
-    if (Platform.isAndroid) return 'http://localhost:5114';
-    return 'http://localhost:5114';
+    //if (kIsWeb) return 'http://localhost:5114';
+    //if (Platform.isAndroid) return 'http://trivia.arkadiuszcios.online/';
+    return 'http://trivia.arkadiuszcios.online';
   }
 
   static String get hubUrl {
